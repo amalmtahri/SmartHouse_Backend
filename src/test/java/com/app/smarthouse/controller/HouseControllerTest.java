2d91f2d7fe97ba72e20fd3bb04912ab766086fe2
ext_mtahri <mtahriamal0@gmail.com>
2022-04-17 20:24:10 +0000

    update

--- src/test/java/com/app/smarthouse/controller/HouseControllerTest.java
+++ src/test/java/com/app/smarthouse/controller/HouseControllerTest.java
@@ -3,10 +3,13 @@ package com.app.smarthouse.controller;
 import com.app.smarthouse.model.Floor;
 import com.app.smarthouse.model.House;
 import com.app.smarthouse.service.impl.HouseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HouseController.class)
@@ -41,7 +45,15 @@ class HouseControllerTest {
     }
 
     @Test
-    void addHouse() {
+    void addHouse() throws Exception {
+        House house1 = new House("12OZUEIEII","HOUSE1","safi",null,null);
+        when(houseService.addHouse(house1))
+                .thenReturn(house1);
+        mockMvc.perform(post("/api/house/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(house1)))
+                .andExpect(status().isOk());
+
     }
 
     @Test
