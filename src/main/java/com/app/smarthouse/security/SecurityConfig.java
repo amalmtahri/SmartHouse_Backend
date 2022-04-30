e3edcd21601dfcd7870fbc74037fca504617db2e
ext_mtahri <mtahriamal0@gmail.com>
2022-04-30 23:49:14 +0000

    add security

---
+++ src/main/java/com/app/smarthouse/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.app.smarthouse.security;
+
+        import com.app.smarthouse.filter.CustomAuthenticationFilter;
+        import com.app.smarthouse.filter.CustomAuthorisationFilter;
+        import com.app.smarthouse.service.impl.UserService;
+        import lombok.RequiredArgsConstructor;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.http.HttpMethod;
+        import org.springframework.security.authentication.AuthenticationManager;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+        import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+        import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    public final UserService userDetailsService;
+
+    public final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        CustomAuthenticationFilter customAuthenticationFilter=new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.formLogin();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/api/user/register").permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/house/**","/api/user/**","/api/room/**").authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorisationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean()throws Exception{
+        return  super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/app/smarthouse/service/impl/UserService.java
+++ src/main/java/com/app/smarthouse/service/impl/UserService.java
@@ -3,16 +3,29 @@ package com.app.smarthouse.service.impl;
 import com.app.smarthouse.model.User;
 import com.app.smarthouse.repository.UserRepository;
 import com.app.smarthouse.service.interfaces.IUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-@Service("userService")
-public class UserService implements IUserService {
+@Service("userService")@RequiredArgsConstructor
 
-    @Autowired
-    private UserRepository userRepository;
+public class UserService implements IUserService , UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
 
     @Override
     public List<User> getAll(){
@@ -27,5 +40,15 @@ public class UserService implements IUserService {
     @Override
     public User getOne(String id){
         return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =this.userRepository.findByUserName(username);
+        Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        //add user roles
+        //if(user.getHouse().getId()!=null)
+        authorities.add(new SimpleGrantedAuthority("user"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),authorities);
     }
 }
